d249481b5a1dfe859bed0194ad9b87c97ada08c5
Rob <robert@abbeybarn.com>
2023-09-10 22:52:33 +0100

    Added the ERD to the Repo, updated DTO to not include HeroId and use the params in url instead

--- my-turborepo/apps/super-api/src/app.controller.ts
+++ my-turborepo/apps/super-api/src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UpdateAccomplaces } from './update-accomplaces.dto';
 
 @Controller()
 export class AppController {
@@ -10,5 +11,10 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Put('/hero/accomplaces/:id')
+  //Can Have a DTO for Id Param Aswell 
+  async updateAccomplaces(@Param('id') id: number, @Body() updateAccomplaces: UpdateAccomplaces) {
+
+  }
 
 }
